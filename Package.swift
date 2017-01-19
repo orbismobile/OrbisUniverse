426dec95f77294f6041ba830f2ec91264ec34ebf
Luis Perez <lpramirez2413@gmail.com>
2017-01-19 15:16:17 -0500

    UPDATE vapor base project

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "OrbisUniverse",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,20 +1,11 @@
 import Vapor
-import VaporMySQL
 
 let drop = Droplet()
-try drop.addProvider(VaporMySQL.Provider.self)
 
-drop.get ("persons") { _ in
-    
-    if let db = drop.database?.driver as? MySQLDriver {
-        let result = try db.raw("SELECT * FROM persons;")
-        return JSON(result)
-    }
-    else {
-        return "Falló la conexión a la bd"
-    }
-    
-    
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
 }
 
 drop.resource("posts", PostController())
