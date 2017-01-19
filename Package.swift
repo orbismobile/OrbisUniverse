e2d1aef8df74a03af0c3aa6fe40ef9f89c13e2a8
Luis Perez <lpramirez2413@gmail.com>
2017-01-19 15:30:51 -0500

    UPDATE Package.swift file to suppor VaporMySQL

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "OrbisUniverse",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
     ],
     exclude: [
         "Config",
